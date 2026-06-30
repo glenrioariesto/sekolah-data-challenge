ba1cd11188394ed250457c0e68286307416cad39
glenrioariesto <glenrioariesto@gmail.com>
2026-06-30 09:20:58 +0700

    fix: add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
+  base: '/sekolah-data-challenge/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
